85ac10ae8e6cc3985be0f0c1b6be7b05b5794c20
Javan Makhmali <javan@javan.us>
2020-11-11 15:01:58 -0500

    Noop on non-darwin platforms

--- electron-setup.js
+++ electron-setup.js
@@ -1,13 +1,11 @@
-if (process.platform == "darwin") {
-  const { app } = require("electron")
-  const observer = require("./electron-observer")
-  const quarantine = require(".")
+const { app } = require("electron")
+const observer = require("./electron-observer")
+const quarantine = require(".")
 
-  observer.on("download-completed", quarantineDownload)
+observer.on("download-completed", quarantineDownload)
 
-  function quarantineDownload(download) {
-    const path = download.getSavePath()
-    const agentName = app.name
-    quarantine(path, agentName)
-  }
+function quarantineDownload(download) {
+  const path = download.getSavePath()
+  const agentName = app.name
+  quarantine(path, agentName)
 }
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const xattr = require("fs-xattr")
-const uuid = require("uuid")
-
 const IS_DARWIN = process.platform == "darwin"
 const IS_MAC_OS = IS_DARWIN && require("os").release() >= "16"
 
 const ATTRIBUTE = "com.apple.quarantine"
 const TYPE = IS_MAC_OS ? "0081" : "0001"
 
+const nottr = { async set(){}, setSync(){} }
+const xattr = IS_DARWIN ? require("fs-xattr") : nottr
+
 function createValue(agentName) {
   const timeStamp = Math.floor(Date.now() / 1000).toString(16)
-  const eventId = uuid.v4().toUpperCase()
+  const eventId = require("uuid").v4().toUpperCase()
   return [ TYPE, timeStamp, agentName, eventId ].join(";")
 }
 
